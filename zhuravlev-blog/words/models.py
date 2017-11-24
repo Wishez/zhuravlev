c9be91cc9df2128e5aa7bdefdd3f8401ad5d4a9a
Filipp Zhuravlev <shiningfinger@list.ru>
2017-11-24 10:28:37 +0300

    Changed the user model.

--- zhuravlev-blog/words/models.py
+++ zhuravlev-blog/words/models.py
@@ -3,21 +3,54 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractBaseUser
 
+class UserManager(models.Manager):
+    use_for_related_fields = True
+
+    def get_words(self):
+        words = []
+
+
+
 class Word(models.Model):
     name = models.CharField(_('Слово'), max_length=20)
 
+    def __str__(self):
+        return self.name
     class Meta:
         db_table = 'words'
         verbose_name = _('Слово')
         verbose_name_plural = _('Слова')
 
 class User(AbstractBaseUser):
+    username = models.CharField(
+        _('Имя пользователя'),
+        max_length=140,
+        default=None,
+        unique=True
+    )
+    email = models.EmailField(
+        _('Email'),
+        max_length=190,
+        unique=True
+    )
     words = models.ManyToManyField(
         Word,
         verbose_name=_('Слова'),
-        related_name='words'
+        related_name='words',
+        blank=True
     )
-
+    date_joined = models.DateTimeField(
+        _('Зарегистрировался'),
+        auto_now_add=True
+    )
+    is_active = models.BooleanField(
+        _('Активный'),
+        default=True
+    )
+    objects = UserManager()
+    USERNAME_FIELD = 'username'
+    def __str__(self):
+        return self.username
     class Meta:
         db_table = 'liked_users'
         verbose_name = _('Пользователь')
