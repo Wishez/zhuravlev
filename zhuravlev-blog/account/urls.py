bb04eacc84929e6c5385004ad13dcb0c9943f45c
Filipp Zhuravlev <shiningfinger@list.ru>
2017-11-29 09:30:29 +0300

    Add additional urls. Those are change_password, change_email, and recover_password.

--- zhuravlev-blog/account/urls.py
+++ zhuravlev-blog/account/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     url(r'^register/$', register, name='register'),
     url(r'^log_in/$', log_in, name='log_in'),
     url(r'^log_out/$', log_out, name='log_out'),
+    url(r'^change_email/$', change_email, name='change_password'),
+    url(r'^change_password/$', change_password, name='change_email'),
+    url(r'^change_email/$', recover_password, name='recover_password'),
 ]
